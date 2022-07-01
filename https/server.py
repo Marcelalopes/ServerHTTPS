cd69ef21d95ce2d5f99f9ede6a2711de3841c321
Marcela <marcelalopes497@gmail.com>
2022-06-30 22:13:21 -0300

    modificando a implementação

--- https/server.py
+++ https/server.py
@@ -1,8 +1,16 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
+class MyHttpServer(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = './pages/index.html'
+        elif self.path == '/page2':
+            self.path = './pages/page2.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
 
+my_object = MyHttpServer
 port = 4443
-httpd = HTTPServer(("", port), SimpleHTTPRequestHandler)
+httpd = HTTPServer(("", port), my_object)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile="./SSL/certificate.pem", keyfile='./SSL/key.pem', server_side=True)
 
 print("Server running on https://localhost:" + str(port))
