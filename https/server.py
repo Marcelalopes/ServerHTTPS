e4a62f3e52663d03f02904248192bb8586ed1c38
Brenne Santos <tchalisantos40@gmail.com>
2022-07-12 18:21:57 -0300

    atualizando

--- https/server.py
+++ https/server.py
@@ -1,27 +1,35 @@
+from cgitb import html
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
+
+# classe que recebe o método "SimpleHTTPRequestHandler" faz o mapeamento dos diretórios que serão utilizados
+# pelo servidor http
 
 
 class MyHttpServer(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.send_response(200)
-            self.path = './index.html'
+            self.path = './pages/index.html'
         elif self.path == '/page2':
-            self.send_response(200)
             self.path = './pages/page2.html'
         else:
-            self.send_response(404)
             self.path = './pages/404.html'
         return SimpleHTTPRequestHandler.do_GET(self)
 
 
+# Variavel que recebe o objeto do servidor http
 my_object = MyHttpServer
-port = 4443
 
-httpd = HTTPServer(("", port), my_object)
+# definindo a porta que será utilizada
+port = 8080
+# inicia o servidor http passando o ip, porta, e o objeto com os diretórios
+httpd = HTTPServer(("localhost", port), my_object)
+
+# Faz a autenticação utilizando os certificado autoassinados e chave criados no open ssl
 httpd.socket = ssl.wrap_socket(
     httpd.socket, certfile="./SSL/certificate.pem", keyfile='./SSL/key.pem', server_side=True)
-print("Servidor rodando na porta" + str(port))
 
+print("Servidor rodando na porta: " + str(port))
+
+# método para processar uma ou várias requisições.
 httpd.serve_forever()
